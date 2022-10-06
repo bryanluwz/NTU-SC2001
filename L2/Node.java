586dfec4bd5a0b25fb94266bebcf726382691aa3
bryanluwz <bryanlu.my@gmail.com>
2022-10-06 10:25:56 +0800

    idk what's going on

---
+++ L2/Node.java
@@ -0,0 +1,19 @@
+/**
+ * Thanks Aloysius for thy class definition
+ */
+public class Node {
+    private int vertex, cost;
+
+    public Node(int vertex, int cost) {
+        this.vertex = vertex;
+        this.cost = cost;
+    }
+
+    public int getVertex() {
+        return vertex;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}
