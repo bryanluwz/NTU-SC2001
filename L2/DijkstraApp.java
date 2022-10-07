6d5fe42b91ea143c1afc91b5e3a1766089815199
bryanluwz <bryanlu.my@gmail.com>
2022-10-07 10:25:40 +0800

    yes

--- L2/DijkstraApp.java
+++ L2/DijkstraApp.java
@@ -1,53 +1,54 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
+import java.io.*;
 
 public class DijkstraApp {
-	
 	static long counterArray = 0;
 	static long counterMinHeap = 0;
-	
-	
+
 	int minDist(int dist[], boolean visited[], int V) {
 		int min = Integer.MAX_VALUE, minIndex = -1;
-		
-		for (int v=0; v<V; v++) {
+
+		for (int v = 0; v < V; v++) {
 			if (visited[v] == false && dist[v] <= min) {
 				min = dist[v];
 				minIndex = v;
 			}
 		}
-		
+
 		return minIndex;
 	}
-	
-	
+
 	void dijkstraArray(int graph[][], int source, int V) {
-		int dist[] = new int [V];
-		boolean visited[] = new boolean [V];
-		
-		for (int i=0; i<V; i++) {
+		int dist[] = new int[V];
+		boolean visited[] = new boolean[V];
+
+		for (int i = 0; i < V; i++) {
 			dist[i] = Integer.MAX_VALUE;
 			visited[i] = false;
- 		}
-		
+		}
+
 		dist[source] = 0;
-		
-		for (int i=0; i<V; i++) {
+
+		for (int i = 0; i < V; i++) {
 			int u = minDist(dist, visited, V);
 			visited[u] = true;
-			
-			for (int v=0; v<V; v++) {
-				if (visited[v] == false && graph[u][v] != 0 && dist[u] != Integer.MAX_VALUE && dist[u] + graph[u][v] < dist[v])
+
+			for (int v = 0; v < V; v++) {
+				if (visited[v] == false && graph[u][v] != 0 && dist[u] != Integer.MAX_VALUE
+						&& dist[u] + graph[u][v] < dist[v])
 					dist[v] = dist[u] + graph[u][v];
 				counterArray++;
 			}
-				
- 		}
-//		uncomment to check if this is working
-//		for (int j=0; j<V; j++) 
-//			System.out.println("Vertex: " + j + " Minimum Distance from Source: " + dist[j]);
+
+		}
+		// uncomment to check if this is working
+		// for (int j=0; j<V; j++)
+		// System.out.println("Vertex: " + j + " Minimum Distance from Source: " +
+		// dist[j]);
 	}
-	
+
 	public static void dijkstraMinHeap(int V, ArrayList<ArrayList<Node>> graph, int source) {
 		int[] dist = new int[V];
 
@@ -70,45 +71,62 @@ public class DijkstraApp {
 				counterMinHeap++;
 			}
 		}
-// 		uncomment to check if this is working
-//		for (int j = 0; j < V; j++)
-//			System.out.println("Vertex: " + j + " Minimum Distance from Source: " + dist[j]);
-		
+		// uncomment to check if this is working
+		// for (int j = 0; j < V; j++)
+		// System.out.println("Vertex: " + j + " Minimum Distance from Source: " +
+		// dist[j]);
 	}
-	
+
 	public static void main(String[] args) {
 		int V;
-		for (V=10; V<=100; V+=10) {
-			ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-			int[][] intGraph = new int[V][V];
 
+		// Doing stuff for the csv thingy
+		List<String[]> dataLines = new ArrayList<>();
+		String[] header = { "V", "Array with Adjacency Matrix Number of Comparisons",
+				"MinHeap with Adjacency List Number of Comparisons", "Difference (array - heap)" };
+		dataLines.add(header);
 
-			intGraph = RandomArray.random2DSquareArray(V, -10, 10);
+		try {
+			for (V = 10; V <= 200; V += 5) {
+				ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+				int[][] intGraph = new int[V][V];
 
-			int source = 0;
-			System.out.println("V = " + V);
-			// array with adjacency matrix
-			DijkstraApp object = new DijkstraApp();
-	        object.dijkstraArray(intGraph, source, V);
-			System.out.println("Array with Adjacency Matrix");
-			System.out.println("Number of comparisons made: " + counterArray);
-			
-			
+				intGraph = RandomArray.random2DSquareArray(V, -10, 10);
 
-			RandomArray.to2DGraph(intGraph, graph); // convert adj matrix to adj list
-			
-			// minheap with adjacency list
-			dijkstraMinHeap(V, graph, source);
-			System.out.println();
-			System.out.println("MinHeap with Adjacency List");
-			System.out.println("Number of comparisons made: " + counterMinHeap);
-			System.out.println();
-			System.out.println("Difference in number of comparisons made (array - minheap): " + (counterArray-counterMinHeap));
-			System.out.println("=============================================");
-			System.out.println();
-			counterArray = 0;
-			counterMinHeap = 0;
+				int source = 0;
+				System.out.println("V = " + V);
+
+				// array with adjacency matrix
+				DijkstraApp object = new DijkstraApp();
+				object.dijkstraArray(intGraph, source, V);
+				System.out.println("Array with Adjacency Matrix");
+				System.out.println("Number of comparisons made: " + counterArray);
+
+				RandomArray.to2DGraph(intGraph, graph); // convert adj matrix to adj list
+
+				// minheap with adjacency list
+				dijkstraMinHeap(V, graph, source);
+				System.out.println();
+				System.out.println("MinHeap with Adjacency List");
+				System.out.println("Number of comparisons made: " + counterMinHeap);
+				System.out.println();
+				System.out.println(
+						"Difference in number of comparisons made (array - minheap): "
+								+ (counterArray - counterMinHeap));
+				System.out.println("=============================================");
+				System.out.println();
+
+				String[] data = { Integer.toString(V), Long.toString(counterArray), Long.toString(counterMinHeap),
+						Long.toString(counterArray - counterMinHeap) };
+				dataLines.add(data);
+
+				counterArray = 0;
+				counterMinHeap = 0;
+			}
+			CSVthingy.givenDataArray_whenConvertToCSV_thenOutputCreated("./data/output.csv", dataLines);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
+
 	}
 }
